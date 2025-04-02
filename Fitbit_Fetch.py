3ca90065de1c8be0c259c98fbd8ad8e617af8c08
Derek Purdy <10130789+derekpurdy@users.noreply.github.com>
2025-04-02 07:39:46 -0400

    Update Fitbit_Fetch.py

--- Fitbit_Fetch.py
+++ Fitbit_Fetch.py
@@ -156,8 +156,8 @@ def Get_New_Access_Token(client_id, client_secret):
     try:
         access_token, refresh_token = load_tokens_from_file()
     except FileNotFoundError:
-        refresh_token = input("No token file found. Please enter a valid refresh token : ")
-    access_token, refresh_token = refresh_fitbit_tokens(client_id, client_secret, refresh_token)
+        # refresh_token = input("No token file found. Please enter a valid refresh token : ")
+        access_token, refresh_token = refresh_fitbit_tokens(client_id, client_secret, refresh_token)
     return access_token
 
 ACCESS_TOKEN = Get_New_Access_Token(client_id, client_secret)
